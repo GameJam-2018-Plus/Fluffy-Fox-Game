a56e794df96d9bc1ac892ce6ea77322f374b02d5
VectorWolf <mr.joe63@googlemail.com>
2018-12-01 09:37:49 +0100

    Add all the stuff while i was sleeping

--- actor.lua
+++ actor.lua
@@ -1,7 +1,44 @@
+require 'hitbox'
+
 Actor = {}
 
-function Actor:new()
+AllActors = {}
+
+function Actor:new(x, y, img, visible=false, vel=5, max_health=20, team=2)
     obj = {}
+    obj.w, obj.h = img:getPixelDimensions()
+    obj.x = x
+    obj.y = ground["walk_height"] - obj.h
+    obj.visible = visible
+    obj.img = img
+    obj.vel = vel
+    obj.state = "idel"
+    obj.facing = true
+    obj.max_health = max_health
+    obj.health = max_health
+    obj.hbox = Hitbox:new(obj.x, obj.y, obj.w, obj.h)
+    obj.team = team
+    obj.hau = false -- if there's currently a strik going on
     self.__index = self
+
+    AllActors.newKey() = {self}
     return setmetatable(obj, self)
+end
+
+function Actor:draw()
+end
+
+function Actor:get_hit()
+    self.health = self.health - 1
+end
+
+function Actor:hit()
+    -- starts hitting movement
+    hit_check() -- Checks, what actors got hit.
+end
+
+function Actor:ki()
+    if self.team ~= 1 then
+        -- KI-Skript
+    end
 end
--- main.lua
+++ main.lua
@@ -1,9 +1,54 @@
-require 'stage'
-require 'hitbox'
+function love.load ()
+	--Backgroundmusic
+	bgm = love.audio.newSource("assets/Queen_of_the_Night.ogg", "stream")
+	bgm:seek(185, "seconds")
+	love.audio.play(bgm)
+	--Backgroundanimation
+	animation = newAnimation(love.graphics.newImage("assets/mainScreenSprite.png"), 1024, 768,0.5)
+end
+
+function love.update ( dt )
+	--bgm update
+	if not bgm:isPlaying() then
+		bgm:stop()
+		bgm:play()
+	end
+	--bga update
+	animation.currentTime = animation.currentTime + dt
+    if animation.currentTime >= animation.duration then
+        animation.currentTime = animation.currentTime - animation.duration
+    end
+	--exit
+    function love.mousepressed( x, y)   
+        if x > 200 and x < 500 and y > 600 and y < 700 then 
+            love.event.quit()
+        end
+    end
+end
 
 function love.draw()
-    b1 = Hitbox:new(0,0,10,10)
-    b2 = Hitbox:new(0,10,10,10)
-    if(b1:collide(b2)) then love.graphics.print("Collide!",400,200) end
-    love.graphics.print("Hello World", 400, 300)
+	--bga draw
+	 local spriteNum = math.floor(animation.currentTime / animation.duration * #animation.quads) + 1
+    love.graphics.draw(animation.spriteSheet, animation.quads[spriteNum])
+	--gui sketch
+	love.graphics.rectangle("line", 0, 0, 1024, 768)
+	love.graphics.rectangle("line", 200, 600, 300, 100)
+end	
+
+function newAnimation(image, width, height, duration)
+    local animation = {}
+    animation.spriteSheet = image;
+    animation.quads = {};
+ 
+    for y = 0, image:getHeight() - height, height do
+        for x = 0, image:getWidth() - width, width do
+            table.insert(animation.quads, love.graphics.newQuad(x, y, width, height, image:getDimensions()))
+        end
+    end
+	
+	animation.duration = duration or 1
+    animation.currentTime = 0
+ 
+    return animation
 end
+
