2134a8614c76ceb3a7ed9aeaf87d33a18d45d36d
exquilibrium <44523875+exquilibrium@users.noreply.github.com>
2018-12-01 20:16:29 +0100

    Add files via upload

--- main.lua
+++ main.lua
@@ -12,6 +12,9 @@ end
 function love.update ( dt )
 	if love.keyboard.isDown("escape") and current_stage ~= 0 then
 		PAUSE = not PAUSE
+		chVol = false
+		show_option = false
+		show_credits = false
 		love.timer.sleep(.42)
 	end
 	if not PAUSE then	
@@ -24,39 +27,98 @@ function love.update ( dt )
 end
 
 function love.draw()
-	--bga draw
-    stage:draw()
-	--gui sketch
-    if current_stage == 0 or PAUSE then
-		menu(PAUSE)
+	if not show_credits then
+		--bga draw
+		stage:draw()
+		--gui sketch
+		if current_stage == 0 or PAUSE then
+			menu(PAUSE)
+		end
+		if current_stage ~= 0 and not PAUSE then
+			function love.mousepressed( x, y) 
+			end
+		elseif show_option then
+			if chVol then
+				love.graphics.rectangle("line", 442, 368, 50, 50)
+				love.graphics.rectangle("line", 540, 368, 50, 50)
+			end
+			function love.mousepressed( x, y) 
+				-- Volumen
+				if x > 336 and x < 694 and y > 352 and y < 419 then 
+					if not chVol then
+						chVol = true
+					end
+				end
+				local volume = love.audio.getVolume( )
+				if x > 442 and x < 492 and y > 368 and y < 418 then
+					if volume < 1 then
+						volume = volume + .05
+						love.audio.setVolume(volume)
+						print("Volume: "..volume)
+					else
+						volume = 1.0
+						love.audio.setVolume(volume)
+						print("Volume: "..volume)
+					end
+				end
+				if x > 540 and x < 590 and y > 368 and y < 418 then
+					if volume > 0 then
+						volume = volume - .05
+						love.audio.setVolume(volume)
+					else
+						volume = 0.0
+						love.audio.setVolume(volume)
+					end
+				end
+				-- CREDITS/CONFIRM
+				if x > 403 and x < 623 and y > 432 and y < 519 then 
+					if chVol then
+						chVol = false
+					else
+						show_credits = true
+					end
+				end
+				-- Back
+				if x > 450 and x < 586 and y > 512 and y < 579 then 
+					show_option = false
+					chVol = false
+				end
+			end
+		end
+	else
+		if love.keyboard.isDown("escape")then
+			chVol = false
+			show_option = true
+			show_credits = false
+			love.timer.sleep(.42)
+		else
+			local dark = love.graphics.newImage("assets/bg/pauseScreen.png")
+			love.graphics.draw(dark)
+			
+			font = love.graphics.newFont("assets/fonts/BrookeS8.ttf" , 180)
+			love.graphics.setFont(font)
+			love.graphics.rectangle("line", 0, 0, 1024, 768)
+			love.graphics.print("CREDITS", 245, 40)
+
+			font = love.graphics.newFont("assets/fonts/BrookeS8.ttf" , 80)
+			love.graphics.setFont(font)
+			love.graphics.print("GRAPHIC", 100, 180+25)
+			love.graphics.print("SOFTWARE", 100, 330+25)
+			love.graphics.print("STORY/DESIGN", 100, 480+25)
+			love.graphics.print("MUSIC", 100, 630+25)
+			
+			font = love.graphics.newFont("assets/fonts/BrookeS8.ttf" , 40)
+			love.graphics.setFont(font)
+			love.graphics.print("Kirill", 600, 670)
+			love.graphics.print("Felix", 600, 300-90)
+			love.graphics.print("Michael", 600, 340-90)
+			love.graphics.print("Leonhard", 600, 450-120)
+			love.graphics.print("Leif", 600, 490-120)
+			love.graphics.print("Daniel", 600, 530-120)
+			love.graphics.print("Lisa", 600, 600-105)
+			love.graphics.print("Vanessa", 600, 640-105)
+			love.graphics.print("Frederic", 600, 680-105)
+		end
 	end
-	if current_stage ~= 0 and not PAUSE then
-		function love.mousepressed( x, y) 
-		end
-	elseif show_option then
-		function love.mousepressed( x, y) 
-			-- Volumen
-			if x > 336 and x < 694 and y > 352 and y < 419 then 
-				if (not pause) then
-					
-				end	
-			end
-			-- CREDITS
-			if x > 403 and x < 623 and y > 432 and y < 519 then 
-				
-			end
-			-- Back
-			if x > 450 and x < 586 and y > 512 and y < 579 then 
-				show_option = false
-			end
-		end
-	-- elseif show_option then
-	-- 	function love.mousepressed( x, y) 
-	-- 	end
-    end
-	
 end	
-
-
-
 
--- stage_0.lua
+++ stage_0.lua
@@ -1,6 +1,6 @@
 function stage_0()
 	local bg = Animation:new("assets/bg/stage_0.png", 1024, 768,0.42)
         local actors = {}
-        table.insert(actors, Actor:new(450,350,"player",5,3,1))
+        table.insert(actors, Actor:new(1000 , -1000, "player", 5,3,1))
         return Stage:new("assets/audio/test.ogg", bg, 200, actors)
 end
