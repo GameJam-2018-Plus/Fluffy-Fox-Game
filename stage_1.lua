f387534033c72606d80362e368312380b8b7f52f
exquilibrium <44523875+exquilibrium@users.noreply.github.com>
2018-12-02 01:09:39 +0100

    new actor constructors for stages

--- stage_1.lua
+++ stage_1.lua
@@ -1,7 +1,6 @@
 function stage_1()
 	local bg = Animation:new("assets/bg/stage_0.png", 4096, 768,1)
         local actors = {}
-        table.insert(actors, Actor:new(10,200,"player",5,3,1))
-        table.insert(actors, Actor:new(200,200,"player",5,3,2))
+        table.insert(actors, g_player)
         return Stage:new("assets/audio/test.ogg", bg, 200, actors)
 end
--- stage_2.lua
+++ stage_2.lua
@@ -1,6 +1,6 @@
 function stage_2()
 	local bg = Animation:new("assets/bg/stage_2.png", 4608, 768,1)
         local actors = {}
-        table.insert(actors, Actor:new(10,10,"image","stuff","stuff","stuff","stuff"))
+        table.insert(actors, g_player)
         return Stage:new("assets/audio/test.ogg", bg, 200, actors)
 end
