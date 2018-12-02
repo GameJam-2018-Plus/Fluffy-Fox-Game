99ccfd38261965dd88a904d4daf6deff1c5c27e2
exquilibrium <44523875+exquilibrium@users.noreply.github.com>
2018-12-02 07:37:53 +0100

    Add files via upload

--- stage_1.lua
+++ stage_1.lua
@@ -1,11 +1,11 @@
 require 'characters'
 function stage_1()
-	local bg = Animation:new("assets/bg/stage_0.png", 4096, 768,1)
+	local bg = Animation:new("assets/bg/stage_1/s11.png", 9216, 768,1)
         local actors = {}
         table.insert(actors, g_player)
         table.insert(actors, g_test)
-	table.insert(actors, g_circ)
+		table.insert(actors, g_circ)
         table.insert(actors, g_maus)
         table.insert(actors, g_brick)
-        return Stage:new("assets/audio/test.ogg", bg, 200, actors)
+        return Stage:new("assets/audio/TitleScreen.ogg", bg, 200, actors)
 end
