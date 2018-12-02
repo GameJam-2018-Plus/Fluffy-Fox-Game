32e23bf29324e49275213ae15ce91286916ef17a
VectorWolf <mr.joe63@googlemail.com>
2018-12-02 04:34:08 +0100

    Push von Daniel

--- actor.lua
+++ actor.lua
@@ -54,6 +54,44 @@ function Actor:update(dt, stage)
             if stage:hit(self:get_box("att")) then self.sounds.hit:play() end
         end
     end
+    local xx = self.x - stage:get_player().x
+    if self.team == 2 then
+        if math.abs(xx) < 350 then
+            xy = xx/math.abs(xx)
+            self.x = self.x - xy*3
+            if xy < 0 then self.facing = true else self.facing = false end
+        end
+    end
+    if self.team == 3 then
+        if self.y + self.vel < 400 then
+            self.y = self.y + self.vel
+            self.vel = self.vel + 1
+        else
+            self.vel = - self.vel
+            self.y = self.y + self.vel
+            self.vel = self.vel + 1
+        end
+        if self.y < 99 then
+            self.vel = 0
+            self.y = 100
+        end
+        if self.xv<0 then self.facing = false else self.facing = true end 
+        self.x = self.x + self.xv
+        if self.x < 5 then self.xv = 5 end
+        if self.x > 4096 - self.w then self.xv = -5 end
+        self:hit()
+    end
+    if self.team == 4 then
+        if xx < 0 then self.facing = true else self.facing = false end
+        if math.abs(xx) < 450 then
+            self:hit(xx)
+        end
+    end
+    if self.team == 5 then
+        self.x = self.x + self.xv
+        self.y = self.y + self.vel
+        self:hit()
+    end
 end
 
 function Actor:move(x)
@@ -84,7 +122,15 @@ function Actor:get_hit()
     if(self.health == 0) then print("Actor died") alive = false self.sounds.ouch:play() end
 end
 
-function Actor:hit()
+function Actor:hit(xx)
+    if xx and self.x<-500 then
+        local r = math.sqrt(40000 + xx*xx)
+        g_brick.xv = xx/r
+        g_brick.vel = 200/r
+        g_brick.x = self.x + self.w/2 + g_brick.xv
+        g_brick.y = self.y + self.h/2 + g_brick.vel
+        g_brick.health = 1
+    end
     if self.anim_state ~= "slash" then
         self.anims[self.anim_state]:reset()
     end
--- stage_1.lua
+++ stage_1.lua
@@ -4,5 +4,8 @@ function stage_1()
         local actors = {}
         table.insert(actors, g_player)
         table.insert(actors, g_test)
+	table.insert(actors, g_circ)
+        table.insert(actors, g_maus)
+        table.insert(actors, g_brick)
         return Stage:new("assets/audio/test.ogg", bg, 200, actors)
 end
